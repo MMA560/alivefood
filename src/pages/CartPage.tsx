e90793537f19cf193a24b6b7b65b5105ffe37937
MMA560 <mo3geza380@gmail.com>
2025-08-29 23:11:29 +0300

    Update 1.0

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -400,6 +400,7 @@ const CartPage = () => {
                       <span className="text-green-600 font-medium">-{formatPrice(appliedDiscount.amount)}</span>
                     </div>
                     <div className="flex justify-between items-center text-sm">
+                      <span className="text-green-700">{isRTL ? 'الشحن:' : 'Shipping:'}</span>
                       <span className="text-green-700">{formatPrice(shippingFee)}</span>
                     </div>
                   </div>
